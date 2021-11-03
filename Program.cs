4e8a130d123849fd3c8c79fc9d54ff305b4e24ca
WachiranPisitchaikul <93566428+WachiranPisitchaikul@users.noreply.github.com>
2021-11-03 14:10:33 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,46 +1,21 @@
 ﻿using System;
 
-namespace lab6Oop
+namespace Turn_base_battle
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Enemy monster1 = new Enemy(40, 15, 2.0f, "Devil");
-            Enemy monster2 = new Enemy(80, 8, 1.5f, "Dark Elf");
+            dualist MinamotoMusashi = new dualist(300, "Miyamoto Musashi", 100, 50);
+            dualist SasakiKojirou = new dualist(300, "Sasaki Kojirou", 100, 50);
 
-            Console.WriteLine("Enemies data and behaviors >>");
+           
 
-                Console.Write("\nEnter Player Name : ");
-                string player = Console.ReadLine();
 
-            Player Hero = new Player(50, 20, 1.0f, "Alive");
-            Console.WriteLine($"Health : {Hero.HpPlayer}, Attack Damage : {Hero.Atk}, Level : {Hero.Level}, Status : {Hero.Status}");
+            Battle.Startfight(MinamotoMusashi, SasakiKojirou);
 
-            Console.WriteLine("\nEnemy Name : {0}", monster1.EnemyName);
-
-            Console.WriteLine($"Health : {monster1.Hp}, Damage : {monster1.Dmg}, Attack Speed : {monster1.AtkSp}");
             
-            monster1.Attack();
-            monster1.TakeDamage();
-            monster1.UlrimateDamage();
-
-            Hero.HpPlayer -= monster1.Dmg;
-            Hero.GetDamage();
-            Hero.PlayerAttack();
             
-            Console.WriteLine("\nEnemy Name : {0}", monster2.EnemyName);
-
-            Console.WriteLine($"Health : {monster2.Hp}, Damage : {monster2.Dmg}, Attack Speed : {monster2.AtkSp}");
-
-            monster2.Attack();
-            monster2.TakeDamage();
-            monster2.UlrimateDamage();
-
-            Hero.HpPlayer -= monster2.Dmg;
-            Hero.GetDamage();
-            Hero.PlayerAttack();
-
         }
     }
 }
---
+++ dualist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Turn_base_battle
+{
+    public class dualist
+    {
+        public string Name { get; private set; }
+        public double AtkMax { get; private set; }
+        public double BlockMax { get; private set; }
+        public double Health { get; set; }
+        Random rnd = new Random();
+
+        public dualist() { }
+        public dualist(int health ,string name,int atkMax,int blockMax) 
+        {
+            this.Health = health;
+            this.Name = name;
+            this.AtkMax = atkMax;
+            this.BlockMax = blockMax;
+        }
+        public int Attack()
+        {
+            return rnd.Next(1, (int)AtkMax);
+        }
+        public int Block()
+        {
+            return rnd.Next(1,(int)BlockMax);
+        }
+
+    }
+}
