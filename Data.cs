ee62cb0d8692cf5cd1baae9ea1f86dbac2993d34
cheqii <85787376+cheqii@users.noreply.github.com>
2021-11-03 16:16:05 +0700

    Data (Base Class)

---
+++ Data.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemsInClass
+{
+    class Data
+    {
+        protected string Name { get; set; }
+        protected double Hp { get; set; }
+        protected double Atk { get; set; }
+        protected double AtkSpeed { get; set; }
+        protected double Def { get; set; }
+        protected int Evade { get; set; }
+
+        public Data () { }
+
+        public Data (string name, double hp, double atk, double atkSpeed, double def, int evade)
+        {
+            this.Name = name;
+            this.Hp = hp;
+            this.Atk = atk;
+            this.AtkSpeed = atkSpeed;
+            this.Def = def;
+            this.Evade = evade;
+        }
+        
+        public string GetName()
+        {
+            return Name;
+        }
+
+    }
+}
