d291d9e0d5ff036027532a7fdd9d5fc7f80c4f2f
cheqii <85787376+cheqii@users.noreply.github.com>
2021-11-17 00:54:52 +0700

    01 item class update 17/11

---
+++ Item.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace itemForClass
+{
+    public enum ElementType
+    {
+        common,
+        fire,
+        water,
+        leaf
+    }
+    class Item : Data
+    {
+        public int CritChance { get; set; }
+        public ElementType WeaponType { get; set; }
+
+        public Item() { }
+        public Item(string name, double hp, double atk, double atkSpeed, double def, int critChance, int evade, ElementType type) : base(name, hp, atk, atkSpeed, def, evade)
+        {
+            this.CritChance = critChance;
+            this.WeaponType = type;
+        }
+
+        // Attach Items
+        public static void AttachItem(string[] party, string[] itemCheck)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < party.Length; i++)
+            {
+                Console.WriteLine(i + 1 + " " + party[i] + " is used " + itemCheck[i]);
+            }
+            Console.WriteLine();
+        }
+
+        // Equip Items
+        public static void EquipItems(string[] itemSlot, string[] itemCheck, string[] party, Sword sword, Wand wand, Axe axe, Item item)
+        {
+            char confirm;
+            int x = 0;
+
+            // show items in inventory
+            ItemInventory.Inventory(sword, wand, axe);
+
+            do
+            {
+                bool check = true;
+                bool yOrN = true;
+                while (check)
+                {
+                    // select items for character
+                    Console.WriteLine("Please Select Item to attach on your character >> " + party[x]);
+
+                    string inputItems = Console.ReadLine().ToLower();
+                    switch (inputItems)
+                    {
+                        case string n when inputItems == itemSlot[0] || inputItems == itemSlot[1] ||
+                                           inputItems == itemSlot[2]:
+                            {
+                                // Check Duplicate Select Items
+
+                                if (inputItems == itemCheck[0] || inputItems == itemCheck[1] ||
+                                    inputItems == itemCheck[2])
+                                {
+                                    Console.WriteLine("\nYou have selected {0} before .", inputItems);
+                                }
+                                else
+                                {
+                                    // confirm items and attach them
+                                    do
+                                    {
+                                        Console.WriteLine("Are you sure to used " + itemCheck[x] + "  ( y / n ) ");
+                                        Char.TryParse(Console.ReadLine().ToLower(), out confirm);
+                                        switch (confirm)
+                                        {
+                                            case 'y':
+                                                {
+                                                    itemCheck[x] = inputItems;
+                                                    Console.WriteLine("Okay ! You Choose " + itemCheck[x] + "     Let's see");
+                                                    x++;
+                                                    check = false;
+                                                    yOrN = false;
+                                                    break;
+                                                }
+                                            case 'Y':
+                                                {
+                                                    goto case 'y';
+                                                }
+                                            case 'n':
+                                                {
+                                                    Console.WriteLine($"Aww. {inputItems} is not your choice      Let's have a look ... ! ");
+                                                    yOrN = false;
+                                                    break;
+                                                }
+                                            case 'N':
+                                                {
+                                                    goto case 'n';
+                                                }
+                                            default:
+                                                {
+                                                    Console.WriteLine("you enter wrong input .");
+                                                    break;
+                                                }
+                                        }
+                                    } while (yOrN);
+                                }
+                                break;
+                            }
+
+                        default:
+                            {
+                                Console.WriteLine("Please enter the items name .");
+                                check = false;
+                                break;
+                            }
+
+                    }
+                    AttachItem(party, itemCheck);
+                }
+            } while (x < party.Length);
+        }
+    }
+
+    class Sword : Item
+    {
+        public Sword(string name, double hp, double atk, double atkSpeed, double def, int critChance, int evade, ElementType type) : base(name, hp, atk, atkSpeed, def, critChance, evade, type)
+        {
+        }
+    }
+
+    class Wand : Item
+    {
+        public Wand(string name, double hp, double atk, double atkSpeed, double def, int critChance, int evade, ElementType type) : base(name, hp, atk, atkSpeed, def, critChance, evade, type)
+        {
+        }
+    }
+
+    class Axe : Item
+    {
+        public Axe(string name, double hp, double atk, double atkSpeed, double def, int critChance, int evade, ElementType type) : base(name, hp, atk, atkSpeed, def, critChance, evade, type)
+        {
+        }
+    }
+}
---
+++ programItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace itemForClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // items constructors
+            Item item = new Item();
+            Sword item1 = new Sword("sword", 0, 20, 5, 0, 10, 20, ElementType.fire);
+            Wand item2 = new Wand("wand", 0, 10, 10, 0, 15, 20, ElementType.leaf);
+            Axe item3 = new Axe("axe", 0, 35, 5, 0, 20, 18, ElementType.water);
+
+            // item array
+            string[] itemSlot = new string[] { item1.Name, item2.Name, item3.Name };
+            string[] itemCheck = new string[itemSlot.Length];
+
+            // party constructors
+            Data tester = new Data();
+            Data dummy = new Data("Knight", 20, 10, 10, 50, 50);
+            Data dummy2 = new Data("Mage", 15, 5, 15, 20, 55);
+            Data dummy3 = new Data("Hunter", 15, 5, 15, 20, 55);
+
+            // party array
+            string[] party = new string[] { dummy.Name, dummy2.Name, dummy3.Name };
+
+
+            // run phase
+
+            Item.EquipItems(itemSlot, itemCheck, party, item1, item2, item3, item);
+            ItemInventory.ItemWithPlayer(itemCheck, party, item1, item2, item3, item, tester);
+            ItemInventory.RemoveItems(itemCheck, party, item1, item2, item3, item, tester);
+            
+        }
+    }
+}
