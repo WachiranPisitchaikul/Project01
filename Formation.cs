d7b04cd9a1a579a6f478a7c47835a86717c796d6
cheqii <85787376+cheqii@users.noreply.github.com>
2021-11-17 19:13:10 +0700

    02 item class

---
+++ Formation.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace itemForClass
+{
+    class Formation : Data
+    {
+        public int critChance { get; set; }
+
+        public Formation(string name, double hp, double atk, double atkSpeed, double def, int critChance, int evade) : base(name, hp, atk, atkSpeed, def, evade)
+        {
+            this.critChance = critChance;
+        }
+
+        // display formation
+        public static void FormationDisplay(Formation form1, Formation form2, Formation form3)
+        {
+            int i = 0;
+            Formation[] showForm = new Formation[] { form1, form2, form3 };
+            Console.WriteLine("=================================== Formation Info ===============================================\n");
+            foreach (Formation formation in showForm)
+            {
+                Console.WriteLine($" formation [{i + 1}] >> buff health : {formation.Hp} atk : {formation.Atk} atkspeed : {formation.AtkSpeed} def : {formation.Def}");
+                i++;
+            }
+            Console.WriteLine("\n=======================================================================================================");
+        }
+
+
+        // select formation
+
+        public static void SelectFormation(Data pCheck, Data p1, Data p2, Data p3, Formation form1, Formation form2, Formation form3, Sword sword, Wand wand, Axe axe, Dagger dagger, Common obj, string[] party, string[] itemCheck)
+        {
+            int x = 0;
+            int chooseForm;
+            Data[] pStat = new Data[] { p1, p2, p3 };
+            FormationDisplay(form1, form2, form3);
+            do
+            {
+                    Console.WriteLine("\nplease select your formation .\n");
+                    Int32.TryParse(Console.ReadLine(), out chooseForm);
+                for (int i = 0; i < party.Length; i++)
+                {
+                    switch (chooseForm)
+                    {
+                        case 1:
+                            {
+                                if (party[i] == p1.Name)
+                                {
+                                    pCheck.Hp = 20;
+                                    pCheck.Atk = 10;
+                                    pCheck.AtkSpeed = 10;
+                                    pCheck.Def = 50;
+                                    pCheck.Evade = 50;
+                                }
+                                else if (party[i] == p2.Name)
+                                {
+                                    pCheck.Hp = 15;
+                                    pCheck.Atk = 5;
+                                    pCheck.AtkSpeed = 15;
+                                    pCheck.Def = 20;
+                                    pCheck.Evade = 55;
+                                }
+                                else if (party[i] == p3.Name)
+                                {
+                                    pCheck.Hp = 15;
+                                    pCheck.Atk = 5;
+                                    pCheck.AtkSpeed = 15;
+                                    pCheck.Def = 20;
+                                    pCheck.Evade = 55;
+                                }
+
+                                if (itemCheck[i] == sword.Name)
+                                {
+                                    // calculate stat 
+                                    pCheck.Atk += sword.Atk;
+                                    pCheck.AtkSpeed += sword.AtkSpeed;
+                                    pCheck.Def += sword.Def;
+                                    obj.CritChance = 10;
+                                    pCheck.Evade += sword.Evade;
+                                }
+                                else if (itemCheck[i] == wand.Name)
+                                {
+                                    pCheck.Atk += wand.Atk;
+                                    pCheck.AtkSpeed += wand.AtkSpeed;
+                                    pCheck.Def += wand.Def;
+                                    obj.CritChance = 5;
+                                    pCheck.Evade += wand.Evade;
+                                }
+                                else if (itemCheck[i] == axe.Name)
+                                {
+                                    pCheck.Atk += axe.Atk;
+                                    pCheck.AtkSpeed += axe.AtkSpeed;
+                                    pCheck.Def += axe.Def;
+                                    obj.CritChance = 15;
+                                    pCheck.Evade += axe.Evade;
+                                }
+                                else if (itemCheck[i] == dagger.Name)
+                                {
+                                    pCheck.Atk += dagger.Atk;
+                                    pCheck.AtkSpeed += dagger.AtkSpeed;
+                                    pCheck.Def += dagger.Def;
+                                    obj.CritChance = 5;
+                                    pCheck.Evade += dagger.Evade;
+                                }
+
+                                // calculate formation
+                                pCheck.Hp += form1.Hp;
+                                pCheck.Atk += form1.Atk;
+                                pCheck.AtkSpeed += form1.AtkSpeed;
+                                pCheck.Def += form1.Def;
+
+                                Console.WriteLine($" Hp: {pCheck.Hp} Atk: {pCheck.Atk} AtkSpeed: {pCheck.AtkSpeed} Def: {pCheck.Def} CritChance: {obj.CritChance} Evade: {pCheck.Evade}\n");
+                                
+                                x++;
+                                break;
+                            }
+                        case 2:
+                            {
+                                if (party[i] == p1.Name)
+                                {
+                                    pCheck.Hp = 20;
+                                    pCheck.Atk = 10;
+                                    pCheck.AtkSpeed = 10;
+                                    pCheck.Def = 50;
+                                    pCheck.Evade = 50;
+                                }
+                                else if (party[i] == p2.Name)
+                                {
+                                    pCheck.Hp = 15;
+                                    pCheck.Atk = 5;
+                                    pCheck.AtkSpeed = 15;
+                                    pCheck.Def = 20;
+                                    pCheck.Evade = 55;
+                                }
+                                else if (party[i] == p3.Name)
+                                {
+                                    pCheck.Hp = 15;
+                                    pCheck.Atk = 5;
+                                    pCheck.AtkSpeed = 15;
+                                    pCheck.Def = 20;
+                                    pCheck.Evade = 55;
+                                }
+
+                                if (itemCheck[i] == sword.Name)
+                                {
+                                    // calculate stat 
+                                    pCheck.Atk += sword.Atk;
+                                    pCheck.AtkSpeed += sword.AtkSpeed;
+                                    pCheck.Def += sword.Def;
+                                    obj.CritChance = 10;
+                                    pCheck.Evade += sword.Evade;
+                                }
+                                else if (itemCheck[i] == wand.Name)
+                                {
+                                    pCheck.Atk += wand.Atk;
+                                    pCheck.AtkSpeed += wand.AtkSpeed;
+                                    pCheck.Def += wand.Def;
+                                    obj.CritChance = 5;
+                                    pCheck.Evade += wand.Evade;
+                                }
+                                else if (itemCheck[i] == axe.Name)
+                                {
+                                    pCheck.Atk += axe.Atk;
+                                    pCheck.AtkSpeed += axe.AtkSpeed;
+                                    pCheck.Def += axe.Def;
+                                    obj.CritChance = 15;
+                                    pCheck.Evade += axe.Evade;
+                                }
+                                else if (itemCheck[i] == dagger.Name)
+                                {
+                                    pCheck.Atk += dagger.Atk;
+                                    pCheck.AtkSpeed += dagger.AtkSpeed;
+                                    pCheck.Def += dagger.Def;
+                                    obj.CritChance = 5;
+                                    pCheck.Evade += dagger.Evade;
+                                }
+
+                                // calculate formation
+                                pCheck.Hp += form2.Hp;
+                                pCheck.Atk += form2.Atk;
+                                pCheck.AtkSpeed += form2.AtkSpeed;
+                                pCheck.Def += form2.Def;
+
+                                Console.WriteLine($" Hp: {pCheck.Hp} Atk: {pCheck.Atk} AtkSpeed: {pCheck.AtkSpeed} Def: {pCheck.Def} CritChance: {obj.CritChance} Evade: {pCheck.Evade}\n");
+                                
+                                x++;
+                                break;
+                            }
+                        case 3:
+                            {
+                                if (party[i] == p1.Name)
+                                {
+                                    pCheck.Hp = 20;
+                                    pCheck.Atk = 10;
+                                    pCheck.AtkSpeed = 10;
+                                    pCheck.Def = 50;
+                                    pCheck.Evade = 50;
+                                }
+                                else if (party[i] == p2.Name)
+                                {
+                                    pCheck.Hp = 15;
+                                    pCheck.Atk = 5;
+                                    pCheck.AtkSpeed = 15;
+                                    pCheck.Def = 20;
+                                    pCheck.Evade = 55;
+                                }
+                                else if (party[i] == p3.Name)
+                                {
+                                    pCheck.Hp = 15;
+                                    pCheck.Atk = 5;
+                                    pCheck.AtkSpeed = 15;
+                                    pCheck.Def = 20;
+                                    pCheck.Evade = 55;
+                                }
+
+                                if (itemCheck[i] == sword.Name)
+                                {
+                                    // calculate stat 
+                                    pCheck.Atk += sword.Atk;
+                                    pCheck.AtkSpeed += sword.AtkSpeed;
+                                    pCheck.Def += sword.Def;
+                                    obj.CritChance = 10;
+                                    pCheck.Evade += sword.Evade;
+                                }
+                                else if (itemCheck[i] == wand.Name)
+                                {
+                                    pCheck.Atk += wand.Atk;
+                                    pCheck.AtkSpeed += wand.AtkSpeed;
+                                    pCheck.Def += wand.Def;
+                                    obj.CritChance = 5;
+                                    pCheck.Evade += wand.Evade;
+                                }
+                                else if (itemCheck[i] == axe.Name)
+                                {
+                                    pCheck.Atk += axe.Atk;
+                                    pCheck.AtkSpeed += axe.AtkSpeed;
+                                    pCheck.Def += axe.Def;
+                                    obj.CritChance = 15;
+                                    pCheck.Evade += axe.Evade;
+                                }
+                                else if (itemCheck[i] == dagger.Name)
+                                {
+                                    pCheck.Atk += dagger.Atk;
+                                    pCheck.AtkSpeed += dagger.AtkSpeed;
+                                    pCheck.Def += dagger.Def;
+                                    obj.CritChance = 5;
+                                    pCheck.Evade += dagger.Evade;
+                                }
+
+                                // calculate formation
+                                pCheck.Hp += form3.Hp;
+                                pCheck.Atk += form3.Atk;
+                                pCheck.AtkSpeed += form3.AtkSpeed;
+                                pCheck.Def += form3.Def;
+
+                                Console.WriteLine($" Hp: {pCheck.Hp} Atk: {pCheck.Atk} AtkSpeed: {pCheck.AtkSpeed} Def: {pCheck.Def} CritChance: {obj.CritChance} Evade: {pCheck.Evade}\n");
+                                
+                                x++;
+                                break;
+                            }
+                        case int what when chooseForm != 1 || chooseForm != 2 || chooseForm != 3 :
+                            { 
+                                Console.WriteLine("\n- please enter the number between 1-3 -");
+                                i = 3;
+                                break;
+                            }
+                    }
+                }
+            } while (x == 0);
+        }
+    }
+}
---
+++ ItemInventory.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace itemForClass
+{
+    class ItemInventory
+    {
+        // show item inventory
+        public static void Inventory(Sword sword, Wand wand, Axe axe, Dagger dagger)
+        {
+            int i = 0;
+            Item[] itemInInventory = new Item[] {sword, wand, axe, dagger};
+            Console.WriteLine("====== Item Inventory ==============================================\n");
+            foreach (Item item in itemInInventory)
+            {
+                Console.WriteLine(i + 1 + $" {item.Name} Atk: {item.Atk} AtkSpeed: {item.AtkSpeed} Def: {item.Def} CritChance: {item.CritChance} Evade: {item.Evade} Type: {item.WeaponType}");
+                i++;
+            }
+            Console.WriteLine("\n==================================================================");
+        }
+        
+
+        // player status after attach items
+        public static void ItemWithPlayer(string[] itemCheck, string[] party, Sword sword, Wand wand, Axe axe, Dagger dagger, Common obj, Data pCheck, Data p1, Data p2, Data p3)
+        {
+            int x = 0;
+
+            Console.WriteLine("\n=======================================================================================================");
+
+                for (int i = 0; i < party.Length; i++)
+                {
+                    Console.WriteLine("\n" + party[i] + " stat after attach " + itemCheck[i] + "\n");
+                    // check information
+                    if (party[i] == p1.Name)
+                    {
+                        pCheck.Hp = 20;
+                        pCheck.Atk = 10;
+                        pCheck.AtkSpeed = 10;
+                        pCheck.Def = 50;
+                        pCheck.Evade = 50;
+                    }
+                    else if (party[i] == p2.Name)
+                    {
+                        pCheck.Hp = 15;
+                        pCheck.Atk = 5;
+                        pCheck.AtkSpeed = 15;
+                        pCheck.Def = 20;
+                        pCheck.Evade = 55;
+                    } else if (party[i] == p3.Name)
+                    {
+                        pCheck.Hp = 15;
+                        pCheck.Atk = 5;
+                        pCheck.AtkSpeed = 15;
+                        pCheck.Def = 20;
+                        pCheck.Evade = 55;
+                    }
+
+                if (itemCheck[i] == sword.Name)
+                {
+                    // calculate stat 
+                    pCheck.Atk += sword.Atk;
+                    pCheck.AtkSpeed += sword.AtkSpeed;
+                    pCheck.Def += sword.Def;
+                    obj.CritChance = 10;
+                    pCheck.Evade += sword.Evade;
+                }
+                else if (itemCheck[i] == wand.Name)
+                {
+                    pCheck.Atk += wand.Atk;
+                    pCheck.AtkSpeed += wand.AtkSpeed;
+                    pCheck.Def += wand.Def;
+                    obj.CritChance = 5;
+                    pCheck.Evade += wand.Evade;
+                }
+                else if (itemCheck[i] == axe.Name)
+                {
+                    pCheck.Atk += axe.Atk;
+                    pCheck.AtkSpeed += axe.AtkSpeed;
+                    pCheck.Def += axe.Def;
+                    obj.CritChance = 15;
+                    pCheck.Evade += axe.Evade;
+                }
+                else if (itemCheck[i] == dagger.Name)
+                {
+                    pCheck.Atk += dagger.Atk;
+                    pCheck.AtkSpeed += dagger.AtkSpeed;
+                    pCheck.Def += dagger.Def;
+                    obj.CritChance = 5;
+                    pCheck.Evade += dagger.Evade;
+                }
+                Console.WriteLine($" Hp: {pCheck.Hp} Atk: {pCheck.Atk} AtkSpeed: {pCheck.AtkSpeed} Def: {pCheck.Def} CritChance: {obj.CritChance} Evade: {pCheck.Evade}\n");
+                    x++;
+                }
+
+            Console.WriteLine("=======================================================================================================");
+        }
+
+
+        // Remove Items
+        public static void RemoveItems(string[] itemCheck, string[] party, Sword sword, Wand wand, Axe axe, Dagger dagger, Data p1, Data p2, Data p3)
+        {
+            Data[] pStat = new Data[] { p1, p2, p3 };
+            Console.WriteLine("\n - After removed items -\n");
+            for (int i = 0; i < itemCheck.Length; i++)
+            {
+                itemCheck[i] = default;
+            }
+            Item.AttachItem(party, itemCheck);
+            Console.WriteLine();
+            try
+            {
+                foreach (Data data in pStat)
+                {
+                    Console.WriteLine($"{data.Name} Hp: {data.Hp} Atk: {data.Atk} AtkSpeed: {data.AtkSpeed} Def: {data.Def} CritChance: 0 Evade: {data.Evade}\n");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
